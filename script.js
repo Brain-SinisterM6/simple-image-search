f44fd51ebc9f9d949fdd5baf49846636c3ee9d6d
sinisterm6 <hakimasterk@outlook.com>
2023-06-08 11:36:43 +0100

    Simple Image Search Project using JavaScript

---
+++ script.js
@@ -0,0 +1,36 @@
+ const accessKey = '5nllmxh6iktBMyo83GiusWgKS1vVyzWpirITdz52EYo';
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");     //when using a class name with query selector always put a dot "."
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+//When creating functions in JavaScript always use the keyword function
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if (page === 1){
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) =>{
+        const imageWrapper = document.createElement('div')
+        imageWrapper.classList.add(search-result)
+        const image = document.createElement('img')
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description
+    });
+}
